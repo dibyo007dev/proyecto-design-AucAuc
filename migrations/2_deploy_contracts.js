efce3e8793c8d51a49b8e51739a020e88d2e5b4c
Dibyo Majumder <dibyomajumder23@gmail.com>
2019-01-19 04:42:02 +0530

    deploy instance safemath

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 var AucSters = artifacts.require("./AucSters.sol");
+var SafeMath = artifacts.require("./SafeMath.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(AucSters, 1000000);
+  deployer.deploy(SafeMath);
 };
