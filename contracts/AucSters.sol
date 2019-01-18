26b929faa58144dde57ad50ed4684ad5541e6c51
Dibyo Majumder <dibyomajumder23@gmail.com>
2019-01-19 00:16:29 +0530

    transfer tokens

--- contracts/AucSters.sol
+++ contracts/AucSters.sol
@@ -3,13 +3,49 @@
 pragma solidity >=0.4.21 <0.6.0;
 
 contract AucSters {
+    // State varables
     uint256 public totalSupply;
+    string public name;
+    string public symbol;
+    string public standard;
 
+    // Mapping
     mapping(address => uint256) public balanceOf;
 
+    // Events
+    event Transfer(
+        address indexed _from,
+        address indexed _to,
+        uint256 _value
+    );
+
+    // Modifiers
+    modifier hasEnoughBalance(uint _value) {
+            // exception if account doesn't have enough balance
+        require(balanceOf[msg.sender] >= _value, "Not enough balance for the transaction");
+        _;
+    }
+
+
     constructor(uint256 _initialSupply) public {
-        balanceOf[msg.sender] = _initialSupply;
-        totalSupply = _initialSupply;
         // Allocate the initial supply to the deployer
+        balanceOf[msg.sender] = _initialSupply;
+        totalSupply = _initialSupply;   
+
+        name = "AucSters";
+        symbol = "ASS";
+        standard = "AucSters v0.1";        
+    }
+
+    // Transfer
+    function transfer(address _to, uint _value) public hasEnoughBalance(_value) returns(bool) {
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+
+        // Trigger a transfer event
+        emit Transfer(msg.sender, _to, _value);
+        
+        // return a boolean
+        return true;
     }
 }
--- test/AucSters.js
+++ test/AucSters.js
@@ -1,7 +1,26 @@
 var AucSters = artifacts.require("../contracts/AucSters.sol");
 
 contract("AucSters", function(accounts) {
-  it("sets the total supply upon deployment", function() {
+  it("initializes the name symbol and standard", () => {
+    return AucSters.deployed()
+      .then(instance => {
+        tokenInstance = instance;
+        return tokenInstance.name();
+      })
+      .then(name => {
+        assert.equal(name, "AucSters", "Sets name correctly");
+        return tokenInstance.symbol();
+      })
+      .then(symbol => {
+        assert.equal(symbol, "ASS", "Sets the symbol to ASS");
+        return tokenInstance.standard();
+      })
+      .then(standard => {
+        assert.equal(standard, "AucSters v0.1", "Standards set correctly");
+      });
+  });
+
+  it("allocates the total supply upon deployment", function() {
     return AucSters.deployed()
       .then(function(instance) {
         tokenInstance = instance;
@@ -20,6 +39,73 @@ contract("AucSters", function(accounts) {
           adminBalance.toNumber(),
           1000000,
           "allocates the totalSupply to the admin when deployed"
+        );
+      });
+  });
+
+  it("transfers the ownership of token", () => {
+    return AucSters.deployed()
+      .then(instance => {
+        tokenInstance = instance;
+        return tokenInstance.transfer.call(accounts[1], 9999999999999);
+      })
+      .then(assert.fail)
+      .catch(err => {
+        //console.log(err);
+        assert(
+          err.message.indexOf("revert") >= 0,
+          "error msg must contain revert"
+        );
+        return tokenInstance.transfer.call(accounts[1], 250000, {
+          from: accounts[0]
+        });
+      })
+      .then(success => {
+        assert.equal(success, true, "trasfer function returns true");
+
+        return tokenInstance.transfer(accounts[1], 250000, {
+          from: accounts[0]
+        });
+      })
+      .then(reciept => {
+        //console.log(reciept);
+        assert.equal(reciept.logs.length, 1, "only one event must trigger");
+        assert.equal(
+          reciept.logs[0].event,
+          "Transfer",
+          "Transfer event must trigger"
+        );
+        assert.equal(
+          reciept.logs[0].args._from,
+          accounts[0],
+          "debited from account[0]"
+        );
+        assert.equal(
+          reciept.logs[0].args._to,
+          accounts[1],
+          "Added to recipients address"
+        );
+        assert.equal(
+          reciept.logs[0].args._value,
+          250000,
+          "debited from account[0]"
+        );
+
+        return tokenInstance.balanceOf(accounts[1]);
+      })
+      .then(balance => {
+        assert.equal(
+          balance.toNumber(),
+          250000,
+          "adds the amount to the recipients account"
+        );
+        return tokenInstance.balanceOf(accounts[0]);
+      })
+      .then(balance => {
+        assert.equal(
+          balance.toNumber(),
+          750000,
+          "balance remaining should be checked"
         );
       });
   });
