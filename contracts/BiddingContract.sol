fabdb566189e1cc519c608408b84fc3c3277d814
Dibyo Majumder <dibyomajumder23@gmail.com>
2019-01-20 00:20:57 +0530

    further structuring

---
+++ contracts/BiddingContract.sol
@@ -0,0 +1,10 @@
+// BiddingContract: Takes care of all the bidding business logic and admin related permissions
+ 
+pragma solidity >=0.4.21 <0.6.0;
+
+// SafeMath for Ops Utils
+import "./SafeMath.sol";
+
+contract BiddingContract {
+    
+}
---
+++ contracts/SupplyChain.sol
@@ -0,0 +1,10 @@
+// SupplyChain : It monitors the Supply of the product to the customer who won the Bid and responsible for initiating refunds and take action in case of any supply nuisance 
+
+pragma solidity >=0.4.21 <0.6.0;
+
+// SafeMath for Ops Utils
+import "./SafeMath.sol";
+
+contract SupplyChain {
+    
+}
